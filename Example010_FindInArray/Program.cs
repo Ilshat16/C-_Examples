9c6630c4c4b2fc695c016e6fdc103a1b3ab83b53
Ilshat <illliat@mail.ru>
2022-11-30 20:55:39 +0300

    Example 010 Find In Array

---
+++ Example010_FindInArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 12, 32, 113, 62, 72, 17, 32, 22, 91, 44 };
+int find = 32;
+int n = array.Length;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find) 
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
