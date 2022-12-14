7f1d6327c650200786693e829927cac6ad7c3714
Ilshat <illliat@mail.ru>
2022-12-14 21:20:57 +0300

    add examples 4 lesson

---
+++ Example014_BivarArray/Program.cs
@@ -0,0 +1,32 @@
+﻿//Методы
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1, 10);
+        }
+    }
+}
+//Создать двумерный массив
+int[,] matrix = new int[5, 8];
+//Распечатать массив
+PrintArray(matrix);
+Console.WriteLine();
+//Заполнение массива
+FillArray(matrix);
+//Распечатать заполненный массив
+PrintArray(matrix);
---
+++ Example015_Paint/Program.cs
@@ -0,0 +1,53 @@
+﻿//Методы
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillImage(int[,] matr, int row, int col)
+{
+    if (matr[row, col] == 0)
+    {
+        matr[row, col] = 1;
+        FillImage(matr, row-1, col);
+        FillImage(matr, row, col-1);
+        FillImage(matr, row+1, col);
+        FillImage(matr, row, col+1);
+    }
+}
+
+void DrawRhombus(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0) / 2; i++)
+    {
+        matr[i, (matr.GetLength(1) / 2) - i] = 1;
+        matr[i, (matr.GetLength(1) / 2) + i] = 1;
+    }
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        matr[matr.GetLength(0) / 2, j] = 1;
+    }
+    // for (int i = matr.GetLength(0) /2; i < matr.GetLength(0); i++)
+    // {
+    //     matr[i, matr.GetLength(1)/2 + i] = 1;
+    //     matr[i, matr.GetLength(1)/2 - i] = 1;
+    // }
+}
+//Создать двумерный массив
+int[,] matrix = new int[17, 17];
+//Создать картинку ромба
+DrawRhombus(matrix);
+//Распечатать массив
+PrintArray(matrix);
+// Console.WriteLine();
+//Заполнение массива
+FillImage(matrix, 7, 8);
+// //Распечатать заполненный массив
+PrintArray(matrix);
